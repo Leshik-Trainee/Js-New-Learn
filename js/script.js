3271be5520725eeedd0e4183f03c8524791e54a0
Leshik <alex.bezrukov2011@yandex.ru>
2023-08-23 13:45:21 +0300

    Условия

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-// Здесь я задал неизменную переменную в виде const (константа), ибо не знаю, будет ли она меняться в будущем. Имя переменной numberOfFlims. Команда prompt задаёт вопрос посетителю страницы. '' - это поле ответа. 
+// const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+// // Здесь я задал неизменную переменную в виде const (константа), ибо не знаю, будет ли она меняться в будущем. Имя переменной numberOfFlims. Команда prompt задаёт вопрос посетителю страницы. '' - это поле ответа. 
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false, 
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false, 
+// };
 
-const a = prompt('Последний просмотренный фильм?', ''),
-        b = prompt('Ваша оценка?'),
-        c = prompt('Последний просмотренный фильм?', ''),
-        d = prompt('Ваша оценка?');
+// const a = prompt('Последний просмотренный фильм?', ''),
+//         b = prompt('Ваша оценка?'),
+//         c = prompt('Последний просмотренный фильм?', ''),
+//         d = prompt('Ваша оценка?');
         
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
         
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
 
 
 
-
+if(4 == 9) {
+    console.log ('Ok!')
+}
 
